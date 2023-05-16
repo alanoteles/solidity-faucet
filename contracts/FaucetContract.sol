066db3add1e3da472b9d256c698d124a47e1d606
Alano Teles <alanoteles@gmail.com>
2023-05-16 20:30:42 -0300

    Prevent duplications

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -4,19 +4,25 @@ pragma solidity >=0.4.22 <0.9.0;
 contract Faucet {
 
     uint public numOfFunders;
-    mapping(uint => address) private funders;
+    mapping(address => bool) private funders;
+    mapping(uint => address) private lutFunders;
 
     receive() external payable {}
 
     function addFunds() external payable {
-        uint index = numOfFunders++;
-        funders[index] = msg.sender;
+        address funder = msg.sender;
+
+        if (!funders[funder]) {
+            uint index = numOfFunders++;
+            funders[funder] = true;
+            lutFunders[index] = funder;
+        }
     }
 
     function getAllFunders() external view returns(address[] memory){
         address[] memory _funders = new address[](numOfFunders);
         for (uint i = 0; i < numOfFunders; i++) {
-            _funders[i] = funders[i];
+            _funders[i] = lutFunders[i];
         }
         
         return _funders;
@@ -24,7 +30,7 @@ contract Faucet {
 
     function getFunderAtIndex(uint8 index) external view returns(address) {
         // address[] memory _funders = getAllFunders();
-        return funders[index];
+        return lutFunders[index];
     }
     
 }
