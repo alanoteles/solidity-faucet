662921bd70fd0f193c61d0050e56e4cb419b00a9
Alano Teles <alanoteles@gmail.com>
2023-05-16 20:11:02 -0300

    getAllFunders

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -3,20 +3,33 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract Faucet {
 
-    address[] public funders;
+    uint public numOfFunders;
+    mapping(uint => address) private funders;
 
     receive() external payable {}
+
     function addFunds() external payable {
-        funders.push(msg.sender);
+        uint index = numOfFunders++;
+        funders[index] = msg.sender;
     }
 
-    function getAllFunders() public view returns(address[] memory){
-        return funders;
+    function getAllFunders() external view returns(address[] memory){
+        address[] memory _funders = new address[](numOfFunders);
+        for (uint i = 0; i < numOfFunders; i++) {
+            _funders[i] = funders[i];
+        }
+        
+        return _funders;
     }
 
     function getFunderAtIndex(uint8 index) external view returns(address) {
-        address[] memory _funders = getAllFunders();
-        return _funders[index];
+        // address[] memory _funders = getAllFunders();
+        return funders[index];
     }
     
 }
+
+// const instance = await Faucet.deployed()
+// instance.addFunds({from: accounts[0], value: "200000000"})
+// instance.addFunds({from: accounts[1], value: "200000000"})
+// instance.getFunderAtIndex(0)
