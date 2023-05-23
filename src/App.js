4d90cd91e7b077a64049cdfe9d49f46f10f41425
Alano Teles <alanoteles@gmail.com>
2023-05-23 11:59:36 -0300

    Added Web3 library

--- src/App.js
+++ src/App.js
@@ -1,6 +1,48 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import Web3 from "web3";
 
 function App() {
+  const [web3Api, setWeb3Api] = useState({
+    provider: null,
+    web3: null
+  })
+
+  useEffect(() => {
+    const loadProvider = async () => {
+      const provider = null;
+      // if we have Metamask browser extension installed, it will use it.
+      if (window.ethereum) {
+        let provider = window.ethereum;
+
+        try {
+          await provider.enable();
+        } catch {
+          console.error("User denied accounts access!")
+        }
+      }
+      // web3 provider, in case there is no Metamask installation or for legacy systems compatibility
+      else if (window.web3) {
+        provider = window.web3.currentProvider
+      }
+      // Ganache local development environment
+      else if (!process.env.production) {
+        provider = new Web3.providers.HttpProvider("http://localhost:7545")
+
+      }
+
+      setWeb3Api({
+        web3: new Web3(provider),
+        provider
+      })
+    }
+
+    loadProvider();
+  }, [])
+
+  console.log(web3Api.web3);
+  
+
   return (
     <>
       <div className="faucet-wrapper">
