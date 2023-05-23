c108d87411ea9c9e52d18e54bde083cb0f861fca
Alano Teles <alanoteles@gmail.com>
2023-05-23 16:56:39 -0300

    Added metamask/detect-provider

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import Web3 from "web3";
+import detectEthereumProvider from '@metamask/detect-provider'
 
 function App() {
   const [web3Api, setWeb3Api] = useState({
@@ -12,31 +13,18 @@ function App() {
 
   useEffect(() => {
     const loadProvider = async () => {
-      let provider = null;
+      const provider = await detectEthereumProvider()
+
       // if we have Metamask browser extension installed, it will use it.
-      if (window.ethereum) {
-        provider = window.ethereum;
-
-        try {
-          await provider.request({ method: "eth_requestAccounts"})
-        } catch {
-          console.error("User denied accounts access!")
-        }
+      if (provider) {
+        provider.request({method: "eth_requestAccounts"})
+        setWeb3Api({
+          web3: new Web3(provider),
+          provider
+        })
+      } else {
+        console.error("Please, install Metamask.")
       }
-      // web3 provider, in case there is no Metamask installation or for legacy systems compatibility
-      else if (window.web3) {
-        provider = window.web3.currentProvider
-      }
-      // Ganache local development environment
-      else if (!process.env.production) {
-        provider = new Web3.providers.HttpProvider("http://localhost:7545")
-
-      }
-
-      setWeb3Api({
-        web3: new Web3(provider),
-        provider
-      })
     }
 
     loadProvider();
